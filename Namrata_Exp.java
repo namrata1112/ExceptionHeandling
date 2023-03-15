d3a0e86afe90537d175078eafd98fcc1abb7acb5
namrata1112 <97783443+namrata1112@users.noreply.github.com>
2023-03-15 20:10:54 +0530

    Add files via upload

---
+++ Namrata_Exp.java
@@ -0,0 +1,9 @@
+package Exception;
+    public class Namrata_Exp extends Exception{
+        public Namrata_Exp(String message)
+            {
+                super(message);
+            }
+        }
+
+
